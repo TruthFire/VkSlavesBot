e81d396b7908c1f1fc1bffdf0ec997131a517f30
TruthFire <truth.fire@yandex.ru>
2021-03-27 15:21:54 +0200

    Start

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# VkSlavesBot
- 
+# vk-slaves-python-bot
---
+++ config.py
@@ -0,0 +1,10 @@
+ settings = {
+    'TOKEN': 'vk_access_token_settings=friends,status&vk_app_id=7794757&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=desktop_web&vk_ref=catalog_recent&vk_ts=1616849991&vk_user_id=93536233&sign=4TGkJidlbrXT-kD6nnGdCYcKY4QIxVPvPjzzWa1BmJw', # ваш токен
+    'ID': 93536233, # ваш ID 
+    'STEAL_TOP': True, # кража ботов у топ игроков
+    'MULTI_STEAL': False, # риск получить временную !блокировку!!!!!!!! (зато пиздить быстрее будет, логично, да?)
+    'TARGETS': [], # цели для кражи рабов (ID's)
+    'MAX_PRICE': 40000, # максимальная цена раба для кражи
+    'ABUSE_SLAVES': True, # продаёт и покупает своих рабов, пока у них не будет 1000 дохода
+    'ABUSE_IF_BALANCE': 10000000 # абуз будет работать, если у вас баланс больше или равен ABUSE_IF_BALANCE
+}
